fe5d5f00f9d8ba152c9452a8a2a414354a83d0ba
alumno <jose.mvb.cc@gmail.com>
2020-09-15 09:48:59 +0200

    15sep

--- Juanra/1semana/JS/ejercicio6.js
+++ Juanra/1semana/JS/ejercicio6.js
@@ -1,10 +1,20 @@
+window.onload = function(){
+console.log("Iniciado");
+    var ask = prompt("Write a number","");
+    resultado=0;
+
+    while(ask =! -1){
+        resultado= ask+ask;
+    }
+    console.log(resultado);
+    
+}
 
 
 
 
 
-
-6.- Write a program that ask you positive numbers (and sum it) with the following functions:
-    - Program finishes when a -1 is introduced.
-    - When the program finishes. show an alert with the total of all the numbers introduced.
-    - Check if the value introduced is a numeric value or not. If not, show an alert and ask for other number.
+// 6.- Write a program that ask you positive numbers (and sum it) with the following functions:
+//     - Program finishes when a -1 is introduced.
+//     - When the program finishes. show an alert with the total of all the numbers introduced.
+//     - Check if the value introduced is a numeric value or not. If not, show an alert and ask for other number.
